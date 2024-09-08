9b1d82111f12806aecf60ce9a74c296b610b97a8
Matt Marko <mattmarko321@gmail.com>
2024-09-08 05:47:21 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { AppBar, List, ListItem, TextField, Button, Container, Checkbox, ListItemButton, ListItemText } from '@mui/material';
+import './App.css';
+import { TodoItem } from './todo-item';
+
+const exampleItems: TodoItem[] = [
+  {
+    id: 1,
+    task: 'stuff',
+    completed: false,
+  },
+  {
+    id: 2,
+    task: 'things',
+    completed: false,
+  }
+]
+
+function App() {
+  const [todoInput, setTodoInput] = useState('');
+  const [todoItems, setTodoItems] = useState(exampleItems);
+
+  const handleTodoInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoInput(event.target.value);
+  };
+
+  const addTodoItem = () => {
+    if (!todoInput) {
+      return;
+    }
+
+    const newTodoItem: TodoItem = {
+      id: todoItems.length + 1,
+      task: todoInput,
+      completed: false,
+    };
+
+    const newTodoItems: TodoItem[] = [...todoItems, newTodoItem];
+
+    setTodoItems(newTodoItems);
+    setTodoInput('');
+  };
+
+  const completeTask = (id: number) => {
+    const completedItem: TodoItem | undefined = todoItems.find((item) => item.id === id);
+
+    if (completedItem) {
+      if (completedItem.completed) {
+        completedItem.completed = false;
+      } else {
+        completedItem.completed = true;
+      }
+
+      const newTodoItems: TodoItem[] = [...todoItems];
+  
+      setTodoItems(newTodoItems);
+    }
+  };
+
+  const deleteTask = (id: number) => {
+    const remainingItems: TodoItem[] = todoItems.filter((item) => item.id !== id);
+
+    setTodoItems(remainingItems);
+  };
+
+  return (
+    <>
+      <AppBar style={{ height: '40px', fontSize: '26px'}}>Archana</AppBar>
+      <Container>
+        <TextField value={todoInput} onChange={handleTodoInputChange} label="Task" variant="outlined"/>
+        <Button 
+          style={{ height: '55px', fontSize: '30px' }}
+          className="addTodoButton" 
+          onClick={addTodoItem}
+          variant="contained"
+        >
+          +
+        </Button>
+      </Container>
+      <List className="TodoItemsList">
+        {todoItems.map(todoItem => {
+          return (
+            <ListItem key={todoItem.id} className="todoItem" divider={true}>
+              <Checkbox 
+                checked={todoItem.completed}
+                onChange={() => completeTask(todoItem.id)}
+              >
+              </Checkbox>
+              <ListItemText>{todoItem.task}</ListItemText>
+              <Button 
+                className="deleteTaskButton"
+                variant="contained" 
+                style={{backgroundColor: 'red', fontSize: '30px', height: '40px'}}
+                onClick={() => deleteTask(todoItem.id)}
+              >
+                -
+              </Button>
+            </ListItem>
+          )
+        })}
+      </List>
+    </>
+  );
+}
+
+export default App
