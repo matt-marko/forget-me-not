ce5600a2467b65dcbfa34c3ddd7e73c212baa099
Matt Marko <mattmarko321@gmail.com>
2024-09-14 09:02:30 -0700

    Fix type for style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,10 @@
 function Footer() {
-  const footerStyle = {
+  const footerStyle: React.CSSProperties  = {
     position: 'fixed',
     bottom: '0', 
     backgroundColor: '#1976d2',
     width: '100%',
     height: '40px',
-    boxShadow: '5px 10px',
   }
 
   return(
