f8693fa33813811a682c9be48f03fcd3d8ef6e57
Matt Marko <mattmarko321@gmail.com>
2024-11-16 07:54:23 -0800

    Remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import './index.css'
 import '@fontsource/roboto/700.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Dashboard from './routes/Dashboard.tsx';
-import Header from './components/header/Header.tsx';
-import Footer from './components/footer/Footer.tsx';
 import Taskboard from './routes/Taskboard.tsx';
 
 const router = createBrowserRouter([
